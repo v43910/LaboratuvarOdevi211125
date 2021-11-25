10cd02cfa1d379ecbda76747681dc509d21d6321
veysel küçükkaraca <v43910@gmail.com>
2021-11-25 23:32:00 +0300

    Add project files.

---
+++ LaboratuvarOdevi211125/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace LaboratuvarOdevi211125
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIsim = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnCikar = new System.Windows.Forms.Button();
+            this.btnAZ = new System.Windows.Forms.Button();
+            this.btnZA = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "(btn1) uyg1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.btnZA);
+            this.groupBox1.Controls.Add(this.btnAZ);
+            this.groupBox1.Controls.Add(this.btnCikar);
+            this.groupBox1.Controls.Add(this.btnEkle);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtIsim);
+            this.groupBox1.Location = new System.Drawing.Point(116, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(264, 388);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "uyg2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "...";
+            // 
+            // txtIsim
+            // 
+            this.txtIsim.Location = new System.Drawing.Point(31, 19);
+            this.txtIsim.Name = "txtIsim";
+            this.txtIsim.Size = new System.Drawing.Size(138, 20);
+            this.txtIsim.TabIndex = 0;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(31, 54);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(69, 28);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnCikar
+            // 
+            this.btnCikar.Location = new System.Drawing.Point(31, 88);
+            this.btnCikar.Name = "btnCikar";
+            this.btnCikar.Size = new System.Drawing.Size(69, 28);
+            this.btnCikar.TabIndex = 2;
+            this.btnCikar.Text = "Çıkar";
+            this.btnCikar.UseVisualStyleBackColor = true;
+            this.btnCikar.Click += new System.EventHandler(this.btnCikar_Click);
+            // 
+            // btnAZ
+            // 
+            this.btnAZ.Location = new System.Drawing.Point(31, 122);
+            this.btnAZ.Name = "btnAZ";
+            this.btnAZ.Size = new System.Drawing.Size(69, 28);
+            this.btnAZ.TabIndex = 2;
+            this.btnAZ.Text = "A -Z";
+            this.btnAZ.UseVisualStyleBackColor = true;
+            this.btnAZ.Click += new System.EventHandler(this.btnAZ_Click);
+            // 
+            // btnZA
+            // 
+            this.btnZA.Location = new System.Drawing.Point(31, 156);
+            this.btnZA.Name = "btnZA";
+            this.btnZA.Size = new System.Drawing.Size(69, 28);
+            this.btnZA.TabIndex = 2;
+            this.btnZA.Text = "Z-A";
+            this.btnZA.UseVisualStyleBackColor = true;
+            this.btnZA.Click += new System.EventHandler(this.btnZA_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(106, 54);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 329);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIsim;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnZA;
+        private System.Windows.Forms.Button btnAZ;
+        private System.Windows.Forms.Button btnCikar;
+        private System.Windows.Forms.Button btnEkle;
+    }
+}
+
---
+++ LaboratuvarOdevi211125/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace LaboratuvarOdevi211125
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rastgele = new Random();
+            int[] sayilar = new int[100];
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                sayilar[i]= rastgele.Next(500);
+            }
+
+            int buyukSayi = 0;
+            int kucukSayi = 500;
+            int ort = 0;
+            foreach (int item in sayilar)
+            {
+                if (item>buyukSayi)
+                {
+                    buyukSayi = item;
+                }
+
+                if (item<kucukSayi)
+                {
+                    kucukSayi = item;
+                }
+                ort = ort + item;
+            }
+            ort = ort / 100;
+            MessageBox.Show("Büyük Sayı : " + buyukSayi + "\nKüçük Sayı : " + kucukSayi + "\nOrtalama : " + ort);
+        }
+
+        ArrayList aList = new ArrayList();
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            aList.Add(txtIsim.Text);
+            _listboxGuncelle();
+        }
+
+
+
+
+        private void _listboxGuncelle() {
+            listBox1.Items.Clear();
+            foreach (string item in aList)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void btnCikar_Click(object sender, EventArgs e)
+        {
+            aList.Remove(txtIsim.Text);
+            _listboxGuncelle();
+        }
+
+        private void btnAZ_Click(object sender, EventArgs e)
+        {
+            aList.Sort();
+            _listboxGuncelle();
+        }
+
+        private void btnZA_Click(object sender, EventArgs e)
+        {
+            aList.Sort();
+            aList.Reverse();
+            _listboxGuncelle();
+        }
+    }
+}
